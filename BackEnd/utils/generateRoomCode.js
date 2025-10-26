d7640ce99e3a7bc9dccd1e67a1cc9c32764ba101
NaveenLotti <naveenlotti02@gmail.com>
2025-10-26 19:47:20 +0530

    Remove backend server and related files
    Deleted all backend source files including controllers, models, routes, middleware, utility functions, environment configuration, and server entry point. This removes the backend implementation from the project.

--- BackEnd/utils/generateRoomCode.js
+++
@@ -1,9 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const generateRoomCode = () => {
-  const alphabet = "0123456789";
-  const nanoid = customAlphabet(alphabet, 6);
-  return nanoid();
-};
-
-export default generateRoomCode;
